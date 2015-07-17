4db391729a5f69b2a639b71283c884920520dc2f
Andrew Nichols <tandrewnichols@gmail.com>
2015-07-17 11:41:03 -0400

    Add something codeclimate can look at

---
+++ tasks/only.js
@@ -0,0 +1,6 @@
+module.exports = function(grunt) {
+  grunt.registerTask('only', require('../package').description, function() {
+    //grunt.config.requires('only.
+    var options = this.options();
+  });
+};
